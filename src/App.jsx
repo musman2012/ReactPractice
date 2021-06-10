8a04d85f2d8199800cf1b7fc51a6c88f2a09fe7a
musman2012 <m.usman.uob@gmail.com>
2021-06-10 17:02:46 +0100

    notes are made dynamic with props

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import React from "react"
 import Header from "./Header"
 import Footer from "./Footer"
 import Note from "./Note"
+import Notes from "./notes"
+
+function createNote(note){
+    return <Note key={note.key} title={note.title} content={note.content}/>
+}
 
 function App() {
     return <div>
         <Header />
+        {Notes.map(createNote)}
+        {/* <Note />
         <Note />
         <Note />
-        <Note />
-        <Note />
+        <Note /> */}
         <Footer />
     </div> 
     
